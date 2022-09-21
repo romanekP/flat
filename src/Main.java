f83296b3f1b87c1414917a5f132b8b8c81c27fe3
romanekP <romanekpawel@gmail.com>
2022-09-21 19:25:23 +0200

    added new getFloor method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Flat mieszkanie1=new Flat (2, "Wroclaw", true);
+        Flat mieszkanie1 = new Flat(2, "Wroclaw", true);
         System.out.println(mieszkanie1.getFloor());
     }
 }
